5586a689c2f420b2f7d2cbea075519c8f511b6e0
C H Leng <simply.90@gmail.com>
2020-08-13 16:31:42 +0800

    Remove inheritance

--- ADProj/Models/ActingDepartmentHead.cs
+++ ADProj/Models/ActingDepartmentHead.cs
@@ -7,13 +7,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ADProj.Models
 {
-    public class ActingDepartmentHead : DepartmentHead
+    public class ActingDepartmentHead
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string DepartmentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
         [Required]
         public DateTime StartDate { get; set; }
         [Required]
         public DateTime EndDate { get; set; }
 
+        public virtual Department Department { get; set; }
 
     }
+        
 }
