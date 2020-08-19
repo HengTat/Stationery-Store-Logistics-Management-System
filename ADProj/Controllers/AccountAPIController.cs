73793cc52cefa707bd395d1bf1539857aa815694
jamesfrh <james.frh@gmail.com>
2020-08-19 17:24:18 +0800

    Account and Request API controllers

---
+++ ADProj/Controllers/AccountAPIController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ADProj.Services;
+using ADProj.Models;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace ADProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountAPIController : ControllerBase
+    {
+        private EmployeeService es;
+
+        public AccountAPIController(EmployeeService es)
+        {
+            this.es = es;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] CustomEmployeeMobile data)
+        {
+            string email = data.Email;
+            string password = data.Password;
+
+            /*ActingDepartmentHead actingDepartmentHead = es.GetActingDepartmentHead(email);
+            if (actingDepartmentHead == null)
+            {
+                Employee employee = es.GetEmployee(email);
+                if (employee == null)
+                {
+                    //return some error
+                }
+                bool pwdcheck = es.PasswordCheck(employee, password);
+                if (pwdcheck == false)
+                {
+                    //return some error
+                }
+                //get employee id for identification 
+
+                string employeeId = employee.Id.ToString();
+                string employeeRole = employee.Role;
+
+                //send data back to mobile
+            }
+            bool pwdcheck2 = es.PasswordCheck(actingDepartmentHead.Employee, password);
+            if (pwdcheck2 == false)
+            {
+                //return some error
+            }
+            //for identification 
+            string actingDeptHead = actingDepartmentHead.EmployeeId.ToString();
+            string actingDeptRole = "ActingHead";
+            //send data back to mobile
+            */
+        }
+    }
+}
---
+++ ADProj/Controllers/RequestAPIController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ADProj.Models;
+using ADProj.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADProj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestAPIController : ControllerBase
+    {
+        private InventoryService inventService;
+        private RequestDetailService requestDetailservice;
+        private RequestServices requestService;
+
+
+        public RequestAPIController(InventoryService inventService, RequestDetailService requestDetailservice, RequestServices requestService)
+        {
+            this.inventService = inventService;
+            this.requestDetailservice = requestDetailservice;
+            this.requestService = requestService;
+        }
+
+
+        public IEnumerable<InventoryItem> Get()
+        {
+            List<InventoryItem> list = new List<InventoryItem>();
+            list = inventService.ItemList();
+            return list;
+        }
+
+        [HttpGet("{id}")]
+        public InventoryItem Get(string id)
+        {
+            List<InventoryItem> list = new List<InventoryItem>();
+            list = inventService.ItemList();
+            return list.FirstOrDefault(i => i.Id == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] CustomRequestDetails data)
+        {
+            //int RequestId = rs.addRequest(employeeId);
+            //requestService.addRequestDetails(RequestId, data);
+        }
+    }
+}
